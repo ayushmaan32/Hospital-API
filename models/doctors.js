b8d69ea217355a91bd0cf85c6547b4c4322ea9d4
ayushmaan <ayushmaanroy.12@gmail.com>
2023-05-03 16:37:56 +0530

    created doctorSchema

---
+++ models/doctors.js
@@ -0,0 +1,25 @@
+const mongooose = require("mongoose");
+
+const doctorSchema = new mongooose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "password should be greater than 6 length characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Doctor = mongooose.model("Doctor", doctorSchema);
+module.exports = Doctor;
