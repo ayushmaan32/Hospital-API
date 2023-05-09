9139aad0479c56c5447d7f3d4f9c4a0a88fcdddc
Ayushmaan roy <118387093+ayushmaan32@users.noreply.github.com>
2023-05-09 11:42:17 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const homeControllers = require("../controllers/homecontroller");
 router.get("/", homeControllers.home);
-//to handle all routes of /doctors url
+//to handle all routes of /api url
 router.use("/api", require("./api"));
 
 module.exports = router;
